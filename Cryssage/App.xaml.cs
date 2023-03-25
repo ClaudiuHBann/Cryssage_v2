0066db3a2fbd6a6c84e8e6222008b33eaac424ff
Claudiu HBann <123uidualcnnamreh@gmail.com>
2023-03-25 22:39:17 +0200

    Add project files.

---
+++ Cryssage/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Cryssage;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ Cryssage/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Cryssage;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}

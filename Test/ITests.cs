8ae9f7898b37cde35ee1441d002ddd0f2347bc41
Claudiu HBann <123uidualcnnamreh@gmail.com>
2023-03-28 23:52:45 +0300

    added an interface and a manager for tests

---
+++ Test/ITests.cs
@@ -0,0 +1,86 @@
+﻿using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+
+namespace Test
+{
+abstract partial class ITests
+{
+    static uint Count = 0;
+
+    protected Queue<Func<bool>> Modules = new();
+
+    protected virtual bool TestModules()
+    {
+        var thisName = ToString();
+        if (thisName != null)
+        {
+            thisName = thisName.Replace("Test.", "");
+        }
+        else
+        {
+            thisName = "?";
+        }
+
+        if (Count++ > 0)
+        {
+            Console.WriteLine();
+        }
+
+        Console.ForegroundColor = ConsoleColor.DarkYellow;
+        Console.WriteLine($"\tPreparing {thisName} Test Modules...");
+        Console.WriteLine($"\tStarting {thisName} Test Modules...");
+
+        while (Modules.Count > 0)
+        {
+            var test = Modules.Dequeue();
+            if (!test())
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"\t{test.Method.Name.Replace("Test.", "")} Test Module FAILED...");
+                Console.ForegroundColor = ConsoleColor.White;
+
+                return false;
+            }
+        }
+
+        Console.WriteLine($"\tEnding {thisName} Test Modules...");
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine($"\t{thisName} Test Modules PASSED...");
+        Console.ForegroundColor = ConsoleColor.White;
+
+        return true;
+    }
+
+    [GeneratedRegex("\\t|\\n|\\r")]
+    private static partial Regex RegexArgumentExpressionWithoutNewLines();
+
+    [GeneratedRegex(@"\s+")]
+    private static partial Regex RegexArgumentExpressionOneContinuouslySpace();
+
+    protected static bool PrintModuleTest(bool expression,
+                                          [CallerArgumentExpression("expression")] string? argumentExpression = null,
+                                          [CallerFilePath] string? filePath = null,
+                                          [CallerLineNumber] int lineNumber = 0)
+    {
+        if (!expression)
+        {
+            string argumentExpressionNew = "argumentExpression";
+            if (argumentExpression != null)
+            {
+                argumentExpressionNew = RegexArgumentExpressionWithoutNewLines().Replace(argumentExpression, "");
+                argumentExpressionNew =
+                    RegexArgumentExpressionOneContinuouslySpace().Replace(argumentExpressionNew, " ");
+            }
+
+            /*Console.WriteLine($"\t\t{filePath}:{lineNumber}\t {argumentExpressionNew} -> {expression}");*/
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"\t\t{argumentExpressionNew} failed...");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        return !expression;
+    }
+
+    public abstract bool Test();
+}
+}
---
+++ Test/ParserTests/MessageTests.cs
@@ -0,0 +1,100 @@
+﻿using System.Diagnostics;
+
+using Parser;
+using Parser.Message;
+using Parser.Message.Header;
+using Parser.Message.Packet;
+
+namespace Test.ParserTests
+{
+class MessageTests : ITests
+{
+    public override bool Test()
+    {
+        Modules.Enqueue(TestMessageManager);
+        Modules.Enqueue(TestMessageConverter);
+
+        return TestModules();
+    }
+
+    static bool TestMessageConverter()
+    {
+        var guid = Guid.NewGuid();
+
+        HeaderMetadata headerMetadataStart = new(guid, Message.Type.PING, 69);
+        var headerMetadataAsBytes = MessageConverter.HeaderMetadataToBytes(headerMetadataStart);
+        var headerMetadataEnd = MessageConverter.BytesToHeaderMetadata(headerMetadataAsBytes);
+        if (PrintModuleTest(headerMetadataStart == headerMetadataEnd, "HeaderMetadata"))
+        {
+            return false;
+        }
+
+        HeaderData headerDataStart = new(guid, 69);
+        var headerDataAsBytes = MessageConverter.HeaderDataToBytes(headerDataStart);
+        var headerDataEnd = MessageConverter.BytesToHeaderData(headerDataAsBytes);
+        if (PrintModuleTest(headerDataStart == headerDataEnd, "HeaderData"))
+        {
+            return false;
+        }
+
+        PacketMetadata packetMetadataStart = new(headerMetadataStart);
+        var packetMetadataAsBytes = MessageConverter.PacketMetadataToBytes(packetMetadataStart);
+        var packetMetadataEnd = MessageConverter.BytesToPacketMetadata(packetMetadataAsBytes);
+        if (PrintModuleTest(packetMetadataStart == packetMetadataEnd, "PacketMetadata"))
+        {
+            return false;
+        }
+
+        // clang-format off
+        PacketData packetDataStart = new(headerDataStart, "69"u8.ToArray());
+        // clang-format on
+        var packetDataAsBytes = MessageConverter.PacketDataToBytes(packetDataStart);
+        var packetDataEnd = MessageConverter.BytesToPacketData(packetDataAsBytes);
+        if (PrintModuleTest(packetDataStart == packetDataEnd, "PacketData"))
+        {
+            return false;
+        }
+
+        byte[] bytes = new byte[1000000];
+        for (int i = 0; i < bytes.Length; i++)
+        {
+            bytes[i] = (byte)i;
+        }
+
+        var packetDatasEnd = MessageConverter.BytesToPacketDatas(bytes);
+        var packetDatasAsBytes = MessageConverter.PacketDatasToBytes(packetDatasEnd);
+
+        if (PrintModuleTest(bytes != packetDatasAsBytes, "MessageConverter"))
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    static bool TestMessageManager()
+    {
+        var gu1d = Guid.NewGuid();
+
+        byte[] dataStart = new byte[1000000];
+        for (int i = 0; i < dataStart.Length; i++)
+        {
+            dataStart[i] = (byte)i;
+        }
+
+        var message = MessageManager.ToMessage(dataStart, Message.Type.TEXT, gu1d);
+        var messageDisassembled = MessageManager.FromMessage(message);
+
+        if (PrintModuleTest(message.PacketMetadata.Header.GUID == messageDisassembled.GUID &&
+                                message.PacketMetadata.Header.Type == messageDisassembled.Type &&
+                                messageDisassembled.Stream != null &&
+                                dataStart.SequenceEqual(messageDisassembled.Stream),
+                            "MessageManager"))
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
+}
